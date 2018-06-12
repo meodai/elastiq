9483ff3be2caff529aa0c0cec98e4796e13b77e4
David Aerne <meodai@gmail.com>
2018-06-12 20:02:13 +0200

    adds quotes

--- src/layouts/default/script.js
+++ src/layouts/default/script.js
@@ -1,13 +1,5 @@
-const ScrollTrigger = require('scrolltrigger-classes');
-
-const throttle = require('throttle-debounce/throttle');
-const debounce = require('throttle-debounce/debounce');
-
-
 const $body = document.querySelector('body');
 const $header = document.querySelector('.js-header');
-const $homeSlideContainer = document.querySelector('.js-snapp-container');
-
 
 throttle(300, function () {
   // Throttled function
