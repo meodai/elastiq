0b5a79c180ff8acdc2aa82b68b82bf996d5a9b48
David Aerne <meodai@gmail.com>
2020-04-28 16:06:06 +0200

    feat(content): adds fonctional watch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const $logo = document.querySelector('[data-logo]');
 const $logoWrap = document.querySelector('.logo');
+const $clock = document.querySelector('[data-clock]');
 
 const scroller = () => {
   const progress = window.scrollY / window.innerHeight;
@@ -7,8 +8,22 @@ const scroller = () => {
   $logo.style.transform = `translate(-50%, calc(-50% - ${progressMax * 40}rem)) scale(${1 + (progressMax * 1.2)}) rotate(${-35 * progress}deg)`;
 
   $logoWrap.style.setProperty('--top', progress * 16 + 'vh');
-}
+};
+
+const setTimeVars = () => {
+  const now = new Date();
+  const timeInMilliseconds = now.getTime() - now.getTimezoneOffset() * 60000;
+  const minuteProgress = (timeInMilliseconds / 1000 / 60) % 60;
+  const hourProgress = (timeInMilliseconds / 1000 / 60 / 60) % 24;
+  const dayProgress = now.getHours() / 24 * 2;
+
+  $clock.style.setProperty('--minute-progress', minuteProgress);
+  $clock.style.setProperty('--hour-progress', hourProgress);
+  $clock.style.setProperty('--day-progress', dayProgress);
+};
 
 window.addEventListener('scroll', (e) => {
   requestAnimationFrame(scroller);
 });
+
+setInterval(setTimeVars, 1000);
