b7c9f797415382f708323c3f04f9baeaf9ce3ed8
David Aerne <meodai@gmail.com>
2020-04-28 02:52:24 +0200

    fix(deeisgn): bit better on mobile

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const $logo = document.querySelector('[data-logo]');
 const scroller = () => {
   const progress = window.scrollY / window.innerHeight;
   const progressMax = Math.min(progress, 1);
-  $logo.style.transform = `translate(-50%, calc(-50% - ${progressMax * 40}rem)) scale(${1 + (progressMax * 1.4)}) rotate(${-35 * progress}deg)`;
+  $logo.style.transform = `translate(-50%, calc(-50% - ${progressMax * 40}rem)) scale(${1 + (progressMax * 1.2)}) rotate(${-35 * progress}deg)`;
 }
 
 window.addEventListener('scroll', (e) => {
